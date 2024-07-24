93be4ed977dd2a0dfca44e5eaf414e83100417c7
Paa Kwesi Ephraim <peekaylink@gmail.com>
2024-07-24 13:05:18 +0000

    first commit

---
+++ src/app/page/page.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './page.component.html',
+  styleUrl: './page.component.css'
+})
+export class PageComponent {
+
+
+  
+
+}
